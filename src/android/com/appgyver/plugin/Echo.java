4dcb0206cb2914baf0c6470cbfae513ab062ce07
Harri Sarsa <harri.sarsa@appgyver.com>
2013-10-18 17:52:08 +0300

    updated Echo plugin to work on Android

--- src/android/com/appgyver/plugin/Echo.java
+++ src/android/com/appgyver/plugin/Echo.java
@@ -1,8 +1,7 @@
 package org.apache.cordova.plugin;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,7 +10,6 @@ import org.json.JSONObject;
  * This class echoes a string called from JavaScript.
  */
 public class Echo extends CordovaPlugin {
-
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("echo")) {
